4641d27451f90e331dbece4980973272004274e6
Fabio Domingos <domfabio25@gmail.com>
2024-05-16 21:13:40 -0300

    Ajustes do Caminho JS no HTML

---
+++ public/js/menu-burger.js
@@ -0,0 +1,7 @@
+function openBurger() {
+    document.getElementById('menu-burger').style.display = 'block'
+}
+
+function closeBurger() {
+    document.getElementById('menu-burger').style.display = 'none'
+}
